d7a6841729fdc8e620321db6218525b494c3bf86
Aleksey Nogay <leshkanogay@gmail.com>
2023-10-08 01:24:29 +0500

    changed burger menu

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 document.getElementById('burger_menu').addEventListener('click', function(){
     let mobileMenu = document.querySelector('.mobile_nav')
     mobileMenu.classList.toggle('active')
+    this.classList.toggle('open')
 })
+
