9ac460b209b7ddd348d7a9fd06f32940fa43bd0a
Aleksey Nogay <leshkanogay@gmail.com>
2023-10-08 15:46:38 +0500

    added listen btn

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+let mobileMenu = document.querySelector('.mobile_nav')
+let call_back = document.querySelector('.call_back')
+
 document.getElementById('burger_menu').addEventListener('click', function(){
-    let mobileMenu = document.querySelector('.mobile_nav')
+    
     mobileMenu.classList.toggle('active')
     this.classList.toggle('open')
 })
 
+document.getElementById('primary').addEventListener('click', () => {
+    
+    call_back.classList.toggle('active')
+    // console.log('hello');
+})
+
+document.getElementById('cross').addEventListener('click', () => {
+    call_back.classList.remove('active')
+})
