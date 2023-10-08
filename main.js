c48cbfe2820da0772f11d26a0930f1a933f30ce8
Aleksey Nogay <leshkanogay@gmail.com>
2023-10-08 20:12:38 +0500

    add event listener on buttons

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 let mobileMenu = document.querySelector('.mobile_nav')
 let call_back = document.querySelector('.call_back')
+let thanks = document.querySelector('.thanks')
 
 document.getElementById('burger_menu').addEventListener('click', function(){
-    
     mobileMenu.classList.toggle('active')
     this.classList.toggle('open')
 })
 
-document.getElementById('primary').addEventListener('click', () => {
-    
+document.getElementById('primary').addEventListener('click', () => {  
     call_back.classList.toggle('active')
-    // console.log('hello');
 })
 
 document.getElementById('cross').addEventListener('click', () => {
     call_back.classList.remove('active')
+})
+
+document.getElementById('submit').addEventListener('click', () => {
+    call_back.classList.remove('active')
+    thanks.classList.toggle('active')
+})
+
+document.getElementById('thanks_btn').addEventListener('click', () => {
+    thanks.classList.remove('active')
 })
