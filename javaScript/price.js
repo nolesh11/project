ab36db6a6438b2b6fd72084d3d8cd4849eec1e8c
Aleksey Nogay <leshkanogay@gmail.com>
2023-10-14 03:14:35 +0500

    added event on btn

--- javaScript/price.js
+++ javaScript/price.js
@@ -1,9 +1,14 @@
 let first_section = document.getElementById('first')
 let second_section = document.getElementById('second')
+let third_section = document.getElementById('third')
 
 document.getElementById('next').addEventListener('click', () => {
     first_section.classList.toggle('close')
     second_section.classList.toggle('active')
+})
+document.getElementById('next_2').addEventListener('click', () => {
+    second_section.classList.remove('active')
+    third_section.classList.toggle('active')
 })
 
 
