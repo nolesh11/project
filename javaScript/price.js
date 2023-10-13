a821575f18f9cf0322825cf712446b517ee158f5
Aleksey Nogay <leshkanogay@gmail.com>
2023-10-14 02:54:03 +0500

    deleted slider

--- javaScript/price.js
+++ javaScript/price.js
@@ -6,25 +6,7 @@ document.getElementById('next').addEventListener('click', () => {
     second_section.classList.toggle('active')
 })
 
-document.addEventListener('DOMContentLoaded', function() {
-    const slider = document.querySelector('.stairs')
-    const slides = document.querySelectorAll('.stair')
 
-    let index = 0
-
-    function show_slides (index) {
-        slider.style.transform = `translateX(-${index * 100}%)`
-    }
-    
-    document.getElementById('prev_btn').addEventListener('click', function() {
-        index = (index + 1) % slides.length;
-        show_slides(index);
-    })
-    document.getElementById('next_btn').addEventListener('click', function() {
-        index = (index - 1 + slides.length) % slides.length;
-        show_slides(index);
-    })
-})
 
 
 
