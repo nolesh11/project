7dfef1a2b9f260370d52c474b4719997f52f188f
Aleksey Nogay <leshkanogay@gmail.com>
2023-10-14 16:18:39 +0500

    finished all btns

--- javaScript/price.js
+++ javaScript/price.js
@@ -4,19 +4,38 @@ let third_section = document.getElementById('third')
 let last_section = document.getElementById('last')
 let first_btn = document.querySelectorAll('.stair_radio_btn')
 
-let arr1 = []
+let data_put = document.getElementsByClassName('data_value')[0]
+let data_put_2 = document.getElementsByClassName('data_value')[1]
+let data_put_3 = document.getElementsByClassName('data_value')[2]
+let data_put_4 = document.getElementsByClassName('data_value')[3]
+let data_put_5 = document.getElementsByClassName('data_value')[4]
+let data_put_6 = document.getElementsByClassName('data_value')[5]
 
 document.getElementById('next').addEventListener('click', () => {
     first_section.classList.toggle('close')
     second_section.classList.toggle('active')
 })
 document.getElementById('next_2').addEventListener('click', () => {
+    
+
     second_section.classList.remove('active')
     third_section.classList.toggle('active')
+
+    let prop = document.getElementsByClassName('prop')[0].value
+    let prop_2 = document.getElementsByClassName('prop')[1].value
+    let prop_3 = document.getElementsByClassName('prop')[2].value
+
+    data_put_3.innerHTML = prop
+    data_put_4.innerHTML = prop_2
+    data_put_5.innerHTML = prop_3
+
+
 })
 document.getElementById('last_btn').addEventListener('click', () => {
     third_section.classList.remove('active')
     last_section.classList.toggle('active')
+
+
 })
 
 first_btn.forEach(function (button) {
@@ -28,12 +47,9 @@ first_btn.forEach(function (button) {
     })
 })
 
-let data_put = document.getElementsByClassName('data_value')[0]
-let data_put_2 = document.getElementsByClassName('data_value')[1]
-let data_put_3 = document.getElementsByClassName('data_value')[2]
-let data_put_4 = document.getElementsByClassName('data_value')[3]
-let data_put_5 = document.getElementsByClassName('data_value')[4]
-let data_put_6 = document.getElementsByClassName('data_value')[5]
+
+
+// 1 section btns
 
 document.getElementById('stair_radio_btn').addEventListener('click', function () {
     let data_type = document.getElementsByClassName('data_type')[0].textContent
@@ -56,13 +72,39 @@ document.getElementById('stair_radio_btn_4').addEventListener('click', function 
     data_put.innerHTML = data_type
 })
 
+// 2 section btns
+
 document.getElementById('type_btn').addEventListener('click', function() {
     let data_stair = document.getElementsByClassName('data_stair')[0].textContent
     data_put_2.innerHTML = data_stair
+
 })
 document.getElementById('type_btn_2').addEventListener('click', function() {
     let data_stair = document.getElementsByClassName('data_stair')[1].textContent
     data_put_2.innerHTML = data_stair
+})
+
+// 3 section btns
+
+document.getElementById('type_material_btn').addEventListener('click', function () {
+    let data_material = document.getElementsByClassName('data_material')[0].textContent
+    data_put_6.innerHTML = data_material
+})
+document.getElementById('type_material_btn_2').addEventListener('click', function () {
+    let data_material = document.getElementsByClassName('data_material')[1].textContent
+    data_put_6.innerHTML = data_material
+})
+document.getElementById('type_material_btn_3').addEventListener('click', function () {
+    let data_material = document.getElementsByClassName('data_material')[2].textContent
+    data_put_6.innerHTML = data_material
+})
+document.getElementById('type_material_btn_4').addEventListener('click', function () {
+    let data_material = document.getElementsByClassName('data_material')[3].textContent
+    data_put_6.innerHTML = data_material
+})
+document.getElementById('type_material_btn_5').addEventListener('click', function () {
+    let data_material = document.getElementsByClassName('data_material')[4``].textContent
+    data_put_6.innerHTML = data_material
 })
 
 
