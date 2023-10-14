3db321535b13112e33e1b27f233c381ffa8b9765
Aleksey Nogay <leshkanogay@gmail.com>
2023-10-14 13:05:46 +0500

    added eventlistenr for btns

--- javaScript/price.js
+++ javaScript/price.js
@@ -2,7 +2,9 @@ let first_section = document.getElementById('first')
 let second_section = document.getElementById('second')
 let third_section = document.getElementById('third')
 let last_section = document.getElementById('last')
+let first_btn = document.querySelectorAll('.stair_radio_btn')
 
+let arr1 = []
 
 document.getElementById('next').addEventListener('click', () => {
     first_section.classList.toggle('close')
@@ -15,6 +17,37 @@ document.getElementById('next_2').addEventListener('click', () => {
 document.getElementById('last_btn').addEventListener('click', () => {
     third_section.classList.remove('active')
     last_section.classList.toggle('active')
+})
+
+first_btn.forEach(function (button) {
+    button.addEventListener('click', function () {
+        first_btn.forEach(function (btn) {
+            btn.style.backgroundColor = '#FFFFFF'
+        })
+        this.style.backgroundColor = '#FF9646'
+    })
+})
+
+let data_put = document.getElementsByClassName('data_value')[0]
+document.getElementById('stair_radio_btn').addEventListener('click', function () {
+    let data_type = document.getElementsByClassName('data_type')[0].textContent
+    data_put.innerHTML = data_type
+})
+document.getElementById('stair_radio_btn_1').addEventListener('click', function () {
+    let data_type = document.getElementsByClassName('data_type')[1].textContent
+    data_put.innerHTML = data_type
+})
+document.getElementById('stair_radio_btn_2').addEventListener('click', function () {
+    let data_type = document.getElementsByClassName('data_type')[2].textContent
+    data_put.innerHTML = data_type
+})
+document.getElementById('stair_radio_btn_3').addEventListener('click', function () {
+    let data_type = document.getElementsByClassName('data_type')[3].textContent
+    data_put.innerHTML = data_type
+})
+document.getElementById('stair_radio_btn_4').addEventListener('click', function () {
+    let data_type = document.getElementsByClassName('data_type')[4].textContent
+    data_put.innerHTML = data_type
 })
 
 
